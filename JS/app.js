96731c9d1ca8fbf45e3bbcc18bcd7e8a837533c6
Константин <kotkat.lv@gmail.com>
2020-10-01 08:44:48 +0300

    commit

--- JS/app.js
+++ JS/app.js
@@ -1,129 +1,113 @@
-"use strict"
+"use strict";
 
-// Задание 1
+// 1.	Написать функцию, которая принимает строку и выводит статистику о ней: количество букв, количество цифр и количество других знаков.
+function getInfoAboutUserString() {
+    let userString = document.getElementById("userString_1").value;
+    let countOfChars = 0,
+        countOfDigits = 0,
+        countOfOtherSymb = 0;
+    userString.split("").forEach(s => {
+        if (!isNaN(s) && s !== " ") ++countOfDigits;
+        else if (s.match(/[а-яА-я]/iu) != null) ++countOfChars;
+        else if (s !== " ") ++countOfOtherSymb;
+    })
 
-// Создать массив «Список покупок». Каждый элемент массива является объектом, который содержит название продукта, необходимое количество и куплен или нет. Написать несколько функций для работы с таким массивом.
-let shoppingList = [
-    { productName: "Телевизор", necessaryCount: 10, isBought: false },
-    { productName: "Смартфон", necessaryCount: 3, isBought: true },
-    { productName: "Наушники", necessaryCount: 9, isBought: false },
-    { productName: "Компьютерная мышь", necessaryCount: 5, isBought: false },
-    { productName: "Компьютерная клавиатура", necessaryCount: 2, isBought: true },
-];
-
-// 1.	Вывод всего списка на экран таким образом, чтобы сначала шли некупленные продукты, а потом – купленные.
-shoppingList.sort(p => p.isBought ? 1 : -1).forEach(p => console.log(p));
-
-// 2.	Добавление покупки в список. Учтите, что при добавлении покупки с уже существующим в списке продуктом, необходимо увеличивать количество в существующей покупке, а не добавлять новую.
-let purchase1 = { productName: "Телевизор", necessaryCount: 2, isBought: false };
-let purchase2 = { productName: "Микрофон", necessaryCount: 1, isBought: true };
-
-function addToShoppingList(purchase, list) {
-    let index = list.findIndex(p => p.productName === purchase.productName);
-    index !== -1 ? list[index].necessaryCount += purchase.necessaryCount : list.push(purchase);
+    document.getElementById("countOfChars").value = countOfChars;
+    document.getElementById("countOfDigits").value = countOfDigits;
+    document.getElementById("countOfOtherSymbols").value = countOfOtherSymb;
 }
 
-addToShoppingList(purchase1, shoppingList);
-addToShoppingList(purchase2, shoppingList);
+// 2.	Написать функцию, которая принимает двузначное число и возвращает его в текстовом виде.Например: 35–тридцать пять, 89–восемьдесят девять, 12–двенадцать.
+function getStringViewOfUserValue() {
+    let userValue = document.getElementById("userValue_1").value;
 
-// 3.	Покупка продукта. Функция принимает название продукта и отмечает его как купленный.
-function productPurchase(puchaseName, list) {
-    let index = list.findIndex(p => p.productName === puchaseName && !p.isBought);
-    index !== -1 ? list[index].isBought = true : alert("Ошибка при отметке!");
-}
-
-productPurchase("Компьютерная мышь", shoppingList);
-
-// Задание 2
-
-// Создать массив, описывающий чек в магазине. Каждый элемент массива состоит из названия товара, количества и цены за единицу товара. Написать следующие функции.
-let receipt = [
-    { productName: "Телевизор", count: 10, cost: 210 },
-    { productName: "Смартфон", count: 3, cost: 590 },
-    { productName: "Наушники", count: 9, cost: 20 },
-    { productName: "Компьютерная мышь", count: 5, cost: 10 },
-    { productName: "Компьютерная клавиатура", count: 2, cost: 70 },
-];
-
-// 1.	Распечатка чека на экран.
-receipt.forEach(r => console.log(r.productName + ", " + r.count + " шт., " + "цeна за единицу: " + r.cost + "."));
-
-// 2.	Подсчет общей суммы покупки
-let sum = 0;
-receipt.forEach(r => sum += (r.cost * r.count));
-console.log("Общая сумма покупки = " + sum + " р.");
-
-// 3.	Получение самой дорогой покупки в чеке.
-console.log("Самая дорогая покупка в чеке - " + receipt.sort((r1, r2) => (r1.cost * r1.count) < (r2.cost * r2.count))[0].productName);
-
-// 4.	Подсчет средней стоимости одного товара в чеке.
-console.log("Средняя стоимость одного товара в чеке: " + (sum / receipt.length).toFixed(2) + " р.");
-
-// Задание 3
-
-// Создать массив css-стилей (цвет, размер шрифта, выравнивание, подчеркивание и т. д.). Каждый элемент массива – это объект, состоящий из двух свойств: название стиля и значение стиля.
-let styles = [
-    { backgroundColor: "background-color: brown;" },
-    { fontSize: "font-size: xx-large;" },
-    { alignment: "text-align: center;" },
-    { underline: "border-bottom: 2px solid green;" }
-];
-
-// Написать функцию, которая принимает массив стилей и текст, и выводит этот текст с помощью document.write() в тегах <p></p>, добавив в открывающий тег атрибут style со всеми стилями, перечисленными в массиве.
-function textOutput(someStyles, text) {
-    let str = "";
-    for (let i = 0; i < styles.length; i++) {
-        for (let style in styles[i]) {
-            str += styles[i][style] + " ";
-        }
+    if (userValue.split("").length !== 2) {
+        alert("Неверное число!");
+        return;
     }
 
-    document.write('<p style=' + `"${str}">` + `${text}</p>`);
+    let valuesFrom_10_To_19 = new Map();
+    valuesFrom_10_To_19.set(10, "Десять").set(11, "Одиннадцать").set(12, "Двенадцать").set(13, "Тринадцать").set(14, "Четырнадцать").set(15, "Пятнадцать").set(16, "Шестнадцать").set(17, "Семнадцать").set(18, "Восемнадцать").set(19, "Девятнацдцать");
+
+    if (valuesFrom_10_To_19.has(+userValue)) {
+        document.getElementById("stringViewOfUserValue").value = valuesFrom_10_To_19.get(+userValue);
+    } else {
+        let firstDigitMap = new Map();
+        let secondDigitMap = new Map();
+        firstDigitMap.set(0, "").set(1, "один").set(2, "два").set(3, "три").set(4, "четыре").set(5, "пять").set(6, "шесть").set(7, "семь").set(8, "восемь").set(9, "девять");
+        secondDigitMap.set(2, "двадцать").set(3, "тридцать").set(4, "сорок").set(5, "пятьдесят").set(6, "шестьдесят").set(7, "семьдесят").set(8, "восемьдесят").set(9, "девяносто");
+        document.getElementById("stringViewOfUserValue").value = secondDigitMap.get(+userValue[0]) + " " + firstDigitMap.get(+userValue[1]);
+    }
 }
 
-textOutput(styles, "Рандомный текст");
-
-// Задание 4
-
-// Создать массив аудиторий академии. Объект-аудитория состоит из названия, количества посадочных мест (от 10 до 20) и названия факультета, для которого она предназначена.
-
-let audiences = [
-    { name: "1-2", countOfSeats: 15, faculty: "Геолого-географический" },
-    { name: "2-1", countOfSeats: 11, faculty: "Математический" },
-    { name: "1-3", countOfSeats: 11, faculty: "Геолого-географический" },
-    { name: "3-5", countOfSeats: 19, faculty: "Физический" },
-    { name: "4-9", countOfSeats: 17, faculty: "Филологический" },
-    { name: "3-1", countOfSeats: 19, faculty: "Физический" },
-    { name: "2-7", countOfSeats: 18, faculty: "Математический" },
-];
-
-// 1.	Вывод на экран всех аудиторий.
-audiences.forEach(a => console.log("Номер аудитории: " + a.name + ". Количество посадочных мест: " + a.countOfSeats + ". Факультет: " + a.faculty));
-
-// 2.	Вывод на экран аудиторий для указанного факультета.
-let userFaculty = "Геолого-географический";
-audiences.forEach(a => { if (a.faculty === userFaculty) console.log(a.name) });
-
-// 3.	Вывод на экран только тех аудиторий, которые подходят для переданной группы. Объект-группа состоит из названия, количества студентов и названия факультета.
-let groups = [
-    { name: "ГЭ-21", countOfStudents: 20, faculty: "Геолого-географический" },
-    { name: "Г-41", countOfStudents: 15, faculty: "Геолого-географический" },
-    { name: "ГР-31", countOfStudents: 17, faculty: "Геолого-географический" },
-    { name: "АГ-22", countOfStudents: 19, faculty: "Математический" },
-    { name: "ВМП-41", countOfStudents: 10, faculty: "Математический" },
-    { name: "ОФ-11", countOfStudents: 19, faculty: "Физический" },
-    { name: "ОФ-21", countOfStudents: 10, faculty: "Физический" },
-    { name: "БЯ-52", countOfStudents: 16, faculty: "Филологический" },
-];
-
-function getAudience(group) {
-    audiences.forEach(a => { if (a.faculty === group.faculty && a.countOfSeats >= group.countOfStudents) console.log(a.name); });
+// 3.	Написать функцию, которая заменяет в полученной строке большие буквы на маленькие, маленькие – на большие, а цифры – на знак нижнего подчеркивания.
+function changeSymbols() {
+    let str = document.getElementById("userString_2").value;
+    str = str.replace(/\d/g, "_").split("");
+    for (let i = 0; i < str.length; i++) {
+        str[i] === str[i].toUpperCase() ? str[i] = str[i].replace(str[i], str[i].toLowerCase()) : str[i] = str[i].replace(str[i], str[i].toUpperCase());
+    }
+    document.getElementById("afterChangedSymb").value = str.join("");
 }
 
-groups.forEach(g => getAudience(g));
+// 4.	Написать функцию, которая преобразует названия css-стилей с дефисом в название в СamelСase стиле: font-size в fontSize, background-color в backgroundColor, text- align в textAlign.
+function transformCSSStyle() {
+    let str = document.getElementById("userString_3").value.split("-");
+    document.getElementById("afterTransforming").value = str[0] + str[1].charAt(0).toUpperCase() + str[1].substring(1, str[1].length);
+}
 
-// 4.	Функция сортировки аудиторий по количеству мест.
-audiences.sort((a1, a2) => a1.countOfSeats - a2.countOfSeats).forEach(a => console.log(a));
+// 5.	Написать функцию, которая принимает словосочетание и превращает его в аббревиатуру. Например: cascading  style  sheets в CSS, объектно-ориентированное программирование в ООП.
+function createAbbreviationFromString() {
+    let result = "";
+    document.getElementById("userString_4").value.split(/-|\s/).forEach(s => result += s.charAt(0).toUpperCase());
+    document.getElementById("abbreviation").value = result;
+}
 
-// 5.	Функция сортировки аудиторий по названию (по алфавиту).
-audiences.sort((a1, a2) => a1.faculty - a2.faculty).forEach(a => console.log(a));
+// 6.	Написать функцию, которая принимает любое количество строк, объединяет их в одну длинную строку и возвращает ее.
+function unionUserStrings() {
+    document.getElementById("userStringsUnion").value = document.getElementById("userString_5").value.replace(/\s/g, "");
+}
+
+// 7.	Написать функцию – калькулятор. Функция принимает строку с примером, определяет, какое действие необходимо выполнить (+ - * /), переводит операнды в числа, решает пример и возвращает результат.
+function getOperationResult() {
+    let tmp = document.getElementById("dropdownList");
+    let leftOperand = +document.getElementById("leftOperand").value;
+    let rightOperand = +document.getElementById("rightOperand").value;
+    document.getElementById("operationResult").value = eval(`${leftOperand} ${tmp.options[tmp.selectedIndex].text} ${rightOperand}`);
+}
+
+// 8.   Написать функцию, которая получает url и выводит под-робную информацию о нем. Например: url “https://itstep.org/ua/about”, информация “протокол: https, домен: itstep.org, путь: /ua/about”.
+function parseURL() {
+    let url = new URL(document.getElementById("userString_6").value);
+    document.getElementById("parsedURL").value = `Протокол ${url.protocol}, домен: ${url.host}, путь: ${url.pathname}`;
+}
+
+// 9.	Написать функцию, которая принимает строку и разделитель(одиночный символ) и возвращает массив подстрок, разбитых с помощью указанного разделителя. Например: строка “10 / 08 / 2020”, разделитель “/”, результат: “10”, “08”, “2020”. Выполняя задание, не используйте функцию split().
+function separateUserString() {
+    let str = document.getElementById("userString_7").value;
+    let sep = document.getElementById("separator").value;
+    let result = [];
+    for (let i = 0, tmp = 0; i < str.length; i++) {
+        if (str[i] === sep) {
+            result.push(str.slice(tmp, i));
+            tmp = ++i;
+        }
+
+        if (i === str.length - 1 && str[--tmp] === sep) {
+            result.push(str.slice(++tmp, str.length));
+        }
+    }
+    document.getElementById("afterSeparation").value = result;
+}
+
+// 10.	Написать функцию вывода текста по заданному шаблону. Функция принимает первым параметром шаблон, в тексте которого может использоваться %, после символа % ука-зывается индекс входного параметра. При выводе вместо %индекс необходимо вывести значение соответствующего входного параметра. Например: print(“Today is % 1 % 2. % 3. % 4”, “Monday”, 10, 8, 2020) должна вывести “Today is Monday 10.8.2020”.
+function makeStringFromTemplate() {
+    let template = document.getElementById("template").value;
+    let params = document.getElementById("params").value.split(",");
+
+    for (let i = 0, j = 1; i < params.length; i++) {
+        template = template.replace(`%${j++}`, params[i].trim());
+    }
+
+    document.getElementById("templateString").value = template;
+}
